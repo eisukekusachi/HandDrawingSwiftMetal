d993c0390f1e0f370a666ca6efe813c58accc44c
EisukeKusachi <eisukekusachi@gmail.com>
2021-11-27 18:57:37 +0900

    Initial Commit

---
+++ HandDrawingSwiftMetal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
