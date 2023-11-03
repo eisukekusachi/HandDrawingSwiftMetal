5be8a722b11fbd1e33a7476a2e4be0d0643d7133
EisukeKusachi <eisukekusachi@gmail.com>
2023-11-03 12:54:48 +0900

    Organized files

---
+++ HandDrawingSwiftMetal/Canvas/Drawing/DrawingTextureProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  DrawingTextureProtocol.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2023/04/01.
+//
+
+import MetalKit
+
+/// This protocol encapsulates a series of actions for drawing a single line on a texture.
+protocol DrawingTextureProtocol {
+    var tool: DrawingTool { get }
+
+    var currentTextures: [MTLTexture?] { get }
+
+    var drawingTexture: MTLTexture? { get }
+
+    var textureSize: CGSize { get }
+
+    func initializeTextures(_ textureSize: CGSize)
+
+    func drawOnDrawingTexture(with iterator: Iterator<TouchPoint>, touchState: TouchState)
+    func mergeDrawingTexture(into dstTexture: MTLTexture)
+
+    func clearDrawingTextures()
+}
---
+++ HandDrawingSwiftMetal/Canvas/Metal/CommandQueueProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  CommandQueueProtocol.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2023/10/14.
+//
+
+import MetalKit
+
+protocol CommandQueueProtocol {
+
+    var queue: MTLCommandQueue { get }
+
+    func getOrCreateCommandBuffer() -> MTLCommandBuffer
+    func disposeCommandBuffer()
+}
