ba2ef7060b97a823e7faf91c6d42e96d7241f369
EisukeKusachi <eisukekusachi@gmail.com>
2024-07-28 15:48:39 +0900

    Added `GrayscaleTexturePoint`

---
+++ HandDrawingSwiftMetal/Canvas/Models/GrayscaleTexturePoint.swift
@@ -0,0 +1,48 @@
+//
+//  GrayscaleTexturePoint.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2024/07/28.
+//
+
+import UIKit
+
+struct GrayscaleTexturePoint: Equatable {
+
+    let location: CGPoint
+    let diameter: CGFloat
+
+    /// Grayscale brightness (0.0 ~ 1.0)
+    let brightness: CGFloat
+
+    let blurSize: CGFloat = 2.0
+
+}
+
+extension GrayscaleTexturePoint {
+
+    init(
+        touchPoint: TouchPoint,
+        diameter: CGFloat
+    ) {
+        self.location = touchPoint.location
+        self.diameter = diameter
+        self.brightness = touchPoint.maximumPossibleForce != 0 ? min(touchPoint.force, 1.0) : 1.0
+    }
+
+}
+
+extension GrayscaleTexturePoint {
+
+    static func average(_ left: Self, _ right: Self) -> Self {
+        .init(
+            location: left.location == right.location ? left.location : CGPoint(
+                x: (left.location.x + right.location.x) * 0.5,
+                y: (left.location.y + right.location.y) * 0.5
+            ),
+            diameter: left.diameter == right.diameter ? left.diameter : (left.diameter + right.diameter) * 0.5,
+            brightness: left.brightness == right.brightness ? left.brightness : (left.brightness + right.brightness) * 0.5
+        )
+    }
+
+}
