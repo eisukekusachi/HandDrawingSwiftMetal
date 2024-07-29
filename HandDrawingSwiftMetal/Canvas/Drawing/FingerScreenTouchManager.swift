3df95a9c18ce8c17e801f2c1770a34d169e84511
EisukeKusachi <eisukekusachi@gmail.com>
2024-07-29 21:39:35 +0900

    Added `FingerScreenTouchManager`,`PencilScreenTouchManager`

---
+++ HandDrawingSwiftMetal/Canvas/Drawing/FingerScreenTouchManager.swift
@@ -0,0 +1,58 @@
+//
+//  FingerScreenTouchManager.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2024/07/29.
+//
+
+import UIKit
+
+final class FingerScreenTouchManager {
+
+    private (set) var touchArrayDictionary: [TouchHashValue: [TouchPoint]] = [:]
+
+}
+
+extension FingerScreenTouchManager {
+    var isEmpty: Bool {
+        touchArrayDictionary.isEmpty
+    }
+
+    func append(
+        event: UIEvent?,
+        in view: UIView
+    ) {
+        event?.allTouches?.forEach { touch in
+            guard touch.type != .pencil else { return }
+
+            let key: TouchHashValue = touch.hashValue
+
+            if touch.phase == .began {
+                touchArrayDictionary[key] = []
+            }
+            touchArrayDictionary[key]?.append(
+                .init(
+                    touch: touch,
+                    view: view
+                )
+            )
+        }
+    }
+
+    func removeIfLastElementMatches(phases conditions: [UITouch.Phase]) {
+        touchArrayDictionary.keys.forEach { key in
+            if let touches = touchArrayDictionary[key] {
+                if let lastTouch = touches.last,
+                   conditions.contains(lastTouch.phase) {
+                    touchArrayDictionary.removeValue(forKey: key)
+                }
+            }
+        }
+    }
+
+    func reset() {
+        touchArrayDictionary = [:]
+    }
+
+}
+
---
+++ HandDrawingSwiftMetal/Canvas/Drawing/PencilScreenTouchManager.swift
@@ -0,0 +1,49 @@
+//
+//  PencilScreenTouchManager.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2024/07/29.
+//
+
+import UIKit
+
+final class PencilScreenTouchManager {
+
+    private var touchArray: [TouchPoint] = []
+
+}
+
+extension PencilScreenTouchManager {
+
+    var isEmpty: Bool {
+        touchArray.isEmpty
+    }
+
+    func append(event: UIEvent?, in view: UIView) {
+        event?.allTouches?.forEach { touch in
+            guard
+                touch.type == .pencil,
+                let coalescedTouches = event?.coalescedTouches(for: touch)
+            else { return }
+
+            coalescedTouches.forEach { coalescedTouch in
+                touchArray.append(
+                    .init(touch: coalescedTouch, view: view)
+                )
+            }
+        }
+    }
+
+    func removeIfLastElementMatches(phases conditions: [UITouch.Phase]) {
+        conditions.forEach { condition in
+            if touchArray.last?.phase == condition {
+                touchArray = []
+            }
+        }
+    }
+
+    func reset() {
+        touchArray = []
+    }
+
+}
