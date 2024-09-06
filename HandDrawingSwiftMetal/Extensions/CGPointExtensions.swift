2b5bea68a9c75a29dc26f96e5dd23d43def906ff
EisukeKusachi <eisukekusachi@gmail.com>
2024-09-06 23:01:03 +0900

    Add `func scale`, `func distance` to `CGPointExtensions`

--- HandDrawingSwiftMetal/Extensions/CGPointExtensions.swift
+++ HandDrawingSwiftMetal/Extensions/CGPointExtensions.swift
@@ -32,4 +32,18 @@ extension CGPoint {
         return point
     }
 
+    func scale(_ sourceSize: CGSize, to destinationSize: CGSize) -> Self {
+        let scaleFrameToTexture = ViewSize.getScaleToFit(sourceSize, to: destinationSize)
+
+        return .init(
+            x: self.x * scaleFrameToTexture,
+            y: self.y * scaleFrameToTexture
+        )
+    }
+
+    func distance(_ to: CGPoint?) -> CGFloat {
+        guard let value = to else { return 0.0 }
+        return sqrt(pow(value.x - x, 2) + pow(value.y - y, 2))
+    }
+
 }
