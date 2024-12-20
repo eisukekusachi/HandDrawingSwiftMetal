b2260447edb956f8ff9c5a3e2db4f95eb0179dd2
EisukeKusachi <eisukekusachi@gmail.com>
2024-12-20 23:13:28 +0900

    Add `Logger`

---
+++ HandDrawingSwiftMetal/Utils/Logger.swift
@@ -0,0 +1,20 @@
+//
+//  Logger.swift
+//  HandDrawingSwiftMetal
+//
+//  Created by Eisuke Kusachi on 2024/12/20.
+//
+
+import Foundation
+import os
+
+public enum Logger {
+    public static let standard: os.Logger = .init(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: LogCategory.standard.rawValue
+    )
+}
+
+private enum LogCategory: String {
+     case standard = "Standard"
+}
