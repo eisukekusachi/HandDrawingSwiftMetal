8f069a68ab51f75f292ba50cbf4a4ce7f3f1d335
EisukeKusachi <eisukekusachi@gmail.com>
2025-07-27 19:08:17 +0900

    Add `static func error()`, `static func info` to `Logger`

--- HandDrawingSwiftMetal/CanvasView/Utils/Logger.swift
+++ HandDrawingSwiftMetal/CanvasView/Utils/Logger.swift
@@ -13,6 +13,26 @@ public enum Logger {
         subsystem: Bundle.main.bundleIdentifier!,
         category: LogCategory.standard.rawValue
     )
+
+    public static func error(
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line,
+        _ error: Error
+    ) {
+        let fileName = (file as NSString).lastPathComponent
+        standard.error("[\(fileName):\(line)] \(function) - \(error)")
+    }
+
+    public static func info(
+        file: String = #file,
+        function: String = #function,
+        line: Int = #line,
+        _ message: String
+    ) {
+        let fileName = (file as NSString).lastPathComponent
+        standard.info("[\(fileName):\(line)] \(function) - \(message)")
+    }
 }
 
 private enum LogCategory: String {
