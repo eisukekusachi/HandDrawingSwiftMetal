fee61596f6ec3f0a8d16c5af6422937366434ae5
EisukeKusachi <eisukekusachi@gmail.com>
2025-08-03 16:32:14 +0900

    Change the access control of `let standard` to `private`

--- CanvasView/Sources/CanvasView/Utils/Logger.swift
+++ CanvasView/Sources/CanvasView/Utils/Logger.swift
@@ -9,7 +9,7 @@ import Foundation
 import os
 
 public enum Logger {
-    public static let standard: os.Logger = .init(
+    private static let standard: os.Logger = .init(
         subsystem: Bundle.main.bundleIdentifier!,
         category: LogCategory.standard.rawValue
     )
