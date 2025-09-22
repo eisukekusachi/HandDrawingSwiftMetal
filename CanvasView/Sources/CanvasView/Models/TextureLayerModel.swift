1fecd52edbf1deba6f27e42eb6d5177f9dafbfc7
Eisuke Kusachi <eisukekusachi@gmail.com>
2025-09-22 20:40:19 +0900

    Update `TextureLayerModel`

--- CanvasView/Sources/CanvasView/Models/TextureLayerModel.swift
+++ CanvasView/Sources/CanvasView/Models/TextureLayerModel.swift
@@ -25,6 +25,49 @@ public final class TextureLayerModel: Identifiable, Codable, Equatable, Sendable
         id.uuidString
     }
 
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case alpha
+        case isVisible
+        // legacy
+        case textureName
+    }
+
+    public convenience init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let id: UUID
+        if let decodedID = try? container.decode(UUID.self, forKey: .id) {
+            id = decodedID
+        } else if let legacyIDString = try? container.decode(String.self, forKey: .textureName),
+                  let legacyID = UUID(uuidString: legacyIDString) {
+            id = legacyID
+        } else {
+            throw DecodingError.keyNotFound(
+                CodingKeys.id,
+                .init(
+                    codingPath: decoder.codingPath,
+                    debugDescription: "Missing `id` or legacy `textureName`."
+                )
+            )
+        }
+
+        let title = try container.decode(String.self, forKey: .title)
+        let alpha = try container.decode(Int.self, forKey: .alpha)
+        let isVisible = try container.decode(Bool.self, forKey: .isVisible)
+
+        self.init(id: id, title: title, alpha: alpha, isVisible: isVisible)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(title, forKey: .title)
+        try container.encode(alpha, forKey: .alpha)
+        try container.encode(isVisible, forKey: .isVisible)
+    }
+
     public init(id: UUID, title: String, alpha: Int, isVisible: Bool) {
         self.id = id
         self.title = title
