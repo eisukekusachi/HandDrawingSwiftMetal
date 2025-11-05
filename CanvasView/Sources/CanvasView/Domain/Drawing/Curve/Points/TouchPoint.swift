3f0fba94e3150377a651f722b7cd81323947c6a7
Eisuke Kusachi <eisukekusachi@gmail.com>
2025-11-05 20:55:04 +0900

    Refactor `TouchPoint`

--- CanvasView/Sources/CanvasView/Domain/Drawing/Curve/Points/TouchPoint.swift
+++ CanvasView/Sources/CanvasView/Domain/Drawing/Curve/Points/TouchPoint.swift
@@ -8,64 +8,62 @@
 import UIKit
 
 /// A `Sendable` snapshot that captures the state of a single `UITouch`
+/// https://developer.apple.com/documentation/uikit/uitouch
 public struct TouchPoint: Equatable, Sendable {
 
-    // MARK: - Touch Basics
-
+    /// The current location of the touch in the coordinate system
     public let location: CGPoint
 
+    /// The previous location of the touch in the coordinate system
     public let previousLocation: CGPoint
 
-    public let preciseLocation: CGPoint
-
-    public let precisePreviousLocation: CGPoint
-
-    /// The current phase of the touch
-    public let phase: UITouch.Phase
-
-    /// The time at which the touch event occurred, relative to system uptime
-    public let timestamp: TimeInterval
-
-    /// The number of taps associated with the touch
-    public let tapCount: Int
-
-    /// The general type of input that generated the touch, such as direct or pencil
-    public let type: UITouch.TouchType
-
-    // MARK: - Force and Pressure
-
-    /// The force of the touch, where `1.0` represents the average touch pressure
-    public let force: CGFloat
-
-    /// The maximum possible force value that can be reported by the device
-    public let maximumPossibleForce: CGFloat
-
-    // MARK: - Touch Estimation
-
-    /// The index used to identify estimated touch properties, if available
-    public let estimationUpdateIndex: NSNumber?
-
-    /// The currently estimated properties for this touch
-    public let estimatedProperties: UITouch.Properties
-
-    /// The properties expected to receive future updates
-    public let estimatedPropertiesExpectingUpdates: UITouch.Properties
-
-    // MARK: - Size and Shape
-
-    /// The radius (in points) of the touch area
+    /// The radius (in points) of the touch
     public let majorRadius: CGFloat
 
-    /// The tolerance (± points) for the reported `majorRadius`
+    /// The tolerance (in points) of the touch’s radius
     public let majorRadiusTolerance: CGFloat
 
-    // MARK: - Orientation
+    /// A precise location for the touch, when available
+    public let preciseLocation: CGPoint
 
-    /// The altitude angle of the touch, in radians
+    /// A precise previous location for the touch, when available
+    public let precisePreviousLocation: CGPoint
+
+    /// The number of times the finger was tapped for this given touch
+    public let tapCount: Int
+
+    /// The time when the touch occurred or when it was last mutated
+    public let timestamp: TimeInterval
+
+    /// The type of touch received
+    public let type: UITouch.TouchType
+
+    /// The phase of the touch
+    public let phase: UITouch.Phase
+
+    /// The force of the touch, where a value of 1.0 represents the force of an average touch (predetermined by the system, not user-specific)
+    public let force: CGFloat
+
+    /// The maximum possible force for a touch
+    public let maximumPossibleForce: CGFloat
+
+    /// The altitude (in radians) of the stylus
     public let altitudeAngle: CGFloat
 
-    /// The roll angle of the touch, in radians
+    /// A unit vector that points in the direction of the azimuth of the stylus
+    public let azimuthUnitVector: CGVector
+
+    /// A value that represents the current barrel-roll angle of Apple Pencil
     public let rollAngle: CGFloat
+
+    /// A set of touch properties whose values contain only estimates
+    public let estimatedProperties: UITouch.Properties
+
+    /// The set of touch properties for which updated values are expected in the future
+    public let estimatedPropertiesExpectingUpdates: UITouch.Properties
+
+    /// An index number that lets you correlate an updated touch with the original touch
+    public let estimationUpdateIndex: NSNumber?
 }
 
 public extension TouchPoint {
@@ -79,35 +77,37 @@ public extension TouchPoint {
 
         self.previousLocation = touch.previousLocation(in: view)
 
+        self.majorRadius = touch.majorRadius
+
+        self.majorRadiusTolerance = touch.majorRadiusTolerance
+
         self.preciseLocation = touch.preciseLocation(in: view)
 
         self.precisePreviousLocation = touch.precisePreviousLocation(in: view)
 
-        self.phase = touch.phase
+        self.tapCount = touch.tapCount
 
         self.timestamp = touch.timestamp
 
-        self.tapCount = touch.tapCount
-
         self.type = touch.type
+
+        self.phase = touch.phase
 
         self.force = touch.force
 
         self.maximumPossibleForce = touch.maximumPossibleForce
 
-        self.estimationUpdateIndex = touch.estimationUpdateIndex
+        self.altitudeAngle = touch.altitudeAngle
+
+        self.azimuthUnitVector = touch.azimuthUnitVector(in: view)
+
+        if #available(iOS 17.5, *) { self.rollAngle = touch.rollAngle } else { self.rollAngle = 0 }
 
         self.estimatedProperties = touch.estimatedProperties
 
         self.estimatedPropertiesExpectingUpdates = touch.estimatedPropertiesExpectingUpdates
 
-        self.majorRadius = touch.majorRadius
-
-        self.majorRadiusTolerance = touch.majorRadiusTolerance
-
-        self.altitudeAngle = touch.altitudeAngle
-
-        if #available(iOS 17.5, *) { self.rollAngle = touch.rollAngle } else { self.rollAngle = 0 }
+        self.estimationUpdateIndex = touch.estimationUpdateIndex
     }
 }
 
