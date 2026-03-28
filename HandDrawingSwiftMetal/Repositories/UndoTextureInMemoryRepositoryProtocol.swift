dddaa75fced2c8398b967a865ae298c1ebe57575
Eisuke Kusachi <eisukekusachi@gmail.com>
2026-03-28 23:47:09 +0900

    Fix a comment

--- HandDrawingSwiftMetal/Repositories/UndoTextureInMemoryRepositoryProtocol.swift
+++ HandDrawingSwiftMetal/Repositories/UndoTextureInMemoryRepositoryProtocol.swift
@@ -22,7 +22,7 @@ protocol UndoTextureInMemoryRepositoryProtocol: AnyObject {
     /// Updates the texture. Since `MTLTexture` is a reference type, this texture must be a new instance
     func updateTexture(newTexture: MTLTexture, for id: UndoTextureId) async throws
 
-    /// Removes the texture for the specified `LayerId`
+    /// Removes the texture for the specified `UndoTextureId`
     func removeTexture(_ id: UndoTextureId) throws
 
     /// Removes all textures
