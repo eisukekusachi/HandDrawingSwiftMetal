162d4629c7d04e557620d30799d383a1235e9d74
Eisuke Kusachi <eisukekusachi@gmail.com>
2026-03-31 21:16:37 +0900

    Update `class DrawingDebouncer`

--- HandDrawingSwiftMetal/Models/Drawing/DrawingDebouncer.swift
+++ HandDrawingSwiftMetal/Models/Drawing/DrawingDebouncer.swift
@@ -5,17 +5,10 @@
 //  Created by Eisuke Kusachi on 2025/11/22.
 //
 
-import Combine
 import Foundation
 
 /// A debouncer to prevent heavy processing from running continuously during drawing
 public final class DrawingDebouncer {
-
-    /// A publisher that emits a Bool to indicate processing and completion states
-    var isProcessing: AnyPublisher<Bool, Never> {
-        isProcessingSubject.eraseToAnyPublisher()
-    }
-    private let isProcessingSubject: PassthroughSubject<Bool, Never> = .init()
 
     private let debouncer: Debouncer
 
@@ -24,16 +17,10 @@ public final class DrawingDebouncer {
     }
 
     public func perform(
-        _ block: @escaping () async throws -> Void
+        _ block: @escaping @Sendable () async throws -> Void
     ) {
-        isProcessingSubject.send(true)
-
         debouncer.perform {
-            Task { [weak self] in
-                defer {
-                    self?.isProcessingSubject.send(false)
-                }
-
+            Task {
                 do {
                     try await block()
                 } catch {
