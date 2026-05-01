9d9ba0385066a5a0c1c5e04a27b4f557c67096e3
Eisuke Kusachi <eisukekusachi@gmail.com>
2026-05-01 23:54:07 +0900

    Update CanvasConfigurationTests

--- CanvasView/Tests/CanvasViewTests/CanvasView/CanvasConfigurationTests.swift
+++ CanvasView/Tests/CanvasViewTests/CanvasView/CanvasConfigurationTests.swift
@@ -13,38 +13,35 @@ import CoreGraphics
 @MainActor
 struct CanvasConfigurationTests {
 
+    typealias Subject = CanvasConfiguration
+
     @Test
     func `If textureSize is nil, the screen size is used`() {
-        let configuration = CanvasConfiguration(textureSize: nil)
+        let subject: Subject = .init(textureSize: nil)
 
-        #expect(configuration.textureSize.equalTo(CanvasConfiguration.screenSize))
+        #expect(subject.textureSize.equalTo(CanvasConfiguration.screenSize))
     }
 
-    @Test
-    func `If textureSize is smaller than the screen size, the specified size is used`() {
-        let smallSize = CGSize(width: 10, height: 10)
-        let configuration = CanvasConfiguration(textureSize: smallSize)
+    @Test(
+        arguments: [
+            CGSize(width: 10, height: 10),
+            CGSize(width: 2000, height: 1500)
+        ]
+    )
+    func `If textureSize is valid, the specified size is used`(textureSize: CGSize) {
+        let subject: Subject = .init(textureSize: textureSize)
 
-        #expect(configuration.textureSize.equalTo(smallSize))
-    }
-    @Test
-    func `If textureSize is valid, the specified size is used`() {
-        let textureSize = CGSize(width: 2000, height: 1500)
-        let configuration = CanvasConfiguration(textureSize: textureSize)
-
-        #expect(configuration.textureSize.equalTo(textureSize))
+        #expect(subject.textureSize.equalTo(textureSize))
     }
 
     @Test
     func `Calling newTextureSize() returns a new configuration instance with the specified size`() {
-        let configuration = CanvasConfiguration(
-            textureSize: .init(width: 1000, height: 1000),
-            backgroundColor: .red
+        let subject: Subject = .init(
+            textureSize: .init(width: 1000, height: 1000)
         )
         let newTextureSize: CGSize = .init(width: 2000, height: 2000)
-        let newConfiguration = configuration.newTextureSize(newTextureSize)
+        let newConfiguration = subject.newTextureSize(newTextureSize)
 
         #expect(newConfiguration.textureSize.equalTo(newTextureSize))
-        #expect(newConfiguration.backgroundColor == .red)
     }
 }
