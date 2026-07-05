b2dee7433ba30248b220629a3227d4b174d43be9
Eisuke Kusachi <eisukekusachi@gmail.com>
2026-07-05 14:02:02 +0900

    Remove `func ==`

--- CanvasView/Sources/CanvasView/Extensions/CGSizeExtensions.swift
+++ CanvasView/Sources/CanvasView/Extensions/CGSizeExtensions.swift
@@ -24,8 +24,4 @@ extension CGSize {
     static func >= (lhs: CGSize, rhs: CGSize) -> Bool {
         return lhs.width * lhs.height >= rhs.width * rhs.height
     }
-
-    static func == (lhs: CGSize, rhs: CGSize) -> Bool {
-        return lhs.width == rhs.width && lhs.height == rhs.height
-    }
 }
